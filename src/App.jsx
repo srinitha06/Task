376f5a405ade84d296277d8b03a2583ca5b54f22
srinitha06 <153302697+srinitha06@users.noreply.github.com>
2025-01-23 09:37:19 +0530

    Day 9 evg

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,12 @@
-import React, { useState } from "react";
-import "./App.css";
+import React from 'react';
+import Magic from './Magic';
+import './App.css';
+
 
 function App() {
-  const [tasks, setTasks] = useState([]);
-  const [taskInput, setTaskInput] = useState("");
-
-  const addTask = () => {
-    if (taskInput.trim()) {
-      setTasks([...tasks, taskInput]);
-      setTaskInput("");
-    }
-  };
-
-  const deleteTask = (index) => {
-    const newTasks = tasks.filter((_, i) => i !== index);
-    setTasks(newTasks);
-  };
-
   return (
-    <div className="container">
-      <h1>STUDY TASKS</h1>
-      <input
-        type="text"
-        value={taskInput}
-        onChange={(e) => setTaskInput(e.target.value)}
-        placeholder="Add a new task..."
-      />
-      <button onClick={addTask}>Add Task</button>
-      <ul>
-        {tasks.map((task, index) => (
-          <li key={index}>
-            {task}
-            <button className="deleteBtn" onClick={() => deleteTask(index)}>
-              X
-            </button>
-          </li>
-        ))}
-      </ul>
+    <div>
+      <Magic/>
     </div>
   );
 }
