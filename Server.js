14e8127e702b05941c9354ff1aba5ad283abceda
srinitha06 <153302697+srinitha06@users.noreply.github.com>
2025-01-27 08:50:27 +0530

    Day 12

--- Server.js
+++ Server.js
@@ -1,6 +1,40 @@
-const http=require ('http');
-const server=http.createServer((req,res)=>
-    res.end("server is running")
+//importing express
+const express=require('express');
+//creating a server
+const server=express();
+//assign a port number
+const port=5000;
+const items=[
+    {id:1,name:'jeans'},
+    {id:2,name:'shirts'},
+    {id:3,name:'t-shirts'}
+
+];
+server.use(express.json());
+server.get('/',(req,res)=>{
+res.end("server is running");
+}
 );
-const port=3000;
-server.listen(port); 
+server.get('/product',(req,res)=>{
+    res.json(items)
+
+})
+server.post('/product',(req,res)=>{
+    newitem={id:items.length+1,name:req.body.name};
+    items.push(newitem);
+    res.status(201).json(newitem);
+});
+server.put('/product/:id',(req,res)=>
+{ const itemid=parseInt(req.params.id);
+    const updateditem=items.findIndex((item)=>item.id===itemid);
+    if(updateditem !== -1){
+        items[updateditem].name=req.body.name;
+        res.json(items[updateditem]);
+    }
+    else{
+        res.status(404).json("item not found in data base");
+    }
+
+});
+server.listen(port,()=>
+console.log(`server is running on http://localhost:${port}`));
